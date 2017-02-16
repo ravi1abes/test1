ed5c8b52e56285f86c5b98c3f3dc0e870fcf129f
ravi1abes <raviyadav987@gmail.com>
2017-02-17 00:39:49 +0530

    asdf

---
+++ src/qsp/MavenTest.java
@@ -0,0 +1,16 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+	@Test
+	public void testcreate()
+	{
+		Reporter.log("hiiiiiiiiiiiiiii",true);
+	}
+		
+
+	}
+
